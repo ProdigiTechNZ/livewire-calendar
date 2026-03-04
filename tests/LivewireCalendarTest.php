16c034b9bb97302348b1e907de226e12c7393450
Si Dawson <4170320+sidawson@users.noreply.github.com>
2026-03-04 13:33:44 +1300

    test: Remove all tests
    they're all defaults. we'll create proper ones

--- tests/LivewireCalendarTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Livewire\Livewire;
-use Prodigi\LivewireCalendar\Tests\Testables\TestCalendar;
-
-it('mounts the component', function () {
-    Livewire::Test(TestCalendar::class)
-        ->assertStatus(200);
-});
--- tests/Pest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Prodigi\LivewireCalendar\Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
